74ba24f3c23fa90eb192a495483c9a5dae0f73c7
Yating Huang <yatinghuang@Yatings-MBP.domain>
2021-10-15 22:34:20 -0700

    The number of votes each candidate got

--- PyPoll.py
+++ PyPoll.py
@@ -6,8 +6,9 @@ import csv
 file_to_load = 'Resources/election_results.csv'
 file_to_save = os.path.join('Analysis', 'election_results.txt')
 
-total_votes = 0   # retrieve the total number of votes
+total_votes = 0             # retrieve the total number of votes
 list_of_candidates = []     # retrieve the list of unique candidates
+candidate_votes = {}        # retrieve the total number of votes each candidate receives
 
 with open(file_to_load, 'r') as election_data:
 
@@ -19,7 +20,7 @@ with open(file_to_load, 'r') as election_data:
     #print(headers)
 
     """
-    Total number of votes cast
+        Total number of votes cast
         A complete list of candidates who received votes
         Total number of votes each candidate received
         Percentage of votes each candidate won
@@ -36,6 +37,17 @@ with open(file_to_load, 'r') as election_data:
         Open the data file.
         If applicable, read and ignore headers.
         Create a loop to read each row, and only care about the 3rd column.
+        Add candidates onto our list if they are not already on there.
+    
+    Pseudocode for total number of votes each candidate received:
+        Create a variable to store the dictionary of unique candidates.
+        Open the data file.
+        If applicable, read and ignore headers.
+        Create a loop to read each row.
+        If they are not on the list already, then add them and set vote value = 1.
+        Otherwise, increment that candidate's vote count by 1.
+
+
     """
 
     # Read each row from the file reader 1 at a time
@@ -44,12 +56,19 @@ with open(file_to_load, 'r') as election_data:
         # print(row)
 
         # Each row is a list, and we want the candidate, which is on index 2
+        # Check to see if candidate (row[2]) is not on our list
         if row[2] not in list_of_candidates:
-            list_of_candidates.append(row[2])
+            list_of_candidates.append(row[2])   # add unique candidate to our list
+            candidate_votes[row[2]] = 1         # candidate is key, num_votes = 1 is value
+        # Otherwise, increment the key-value pair associated with the candidate by 1
+        else:
+            candidate_votes[row[2]] += 1        # increment by 1
 
 
 print(f'The total number of votes cast is {total_votes}')
 print(f'The list of candidates: {list_of_candidates}')
+print(f'The number of votes each candidate received: {candidate_votes}')
+
 
 
     
