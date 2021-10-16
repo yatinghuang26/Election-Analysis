837455bd94173f0e9ce36cb55dce53fb8504d146
Yating Huang <yatinghuang@Yatings-MBP.domain>
2021-10-15 21:38:16 -0700

    List of Candidates Who Received Votes

--- PyPoll.py
+++ PyPoll.py
@@ -7,6 +7,7 @@ file_to_load = 'Resources/election_results.csv'
 file_to_save = os.path.join('Analysis', 'election_results.txt')
 
 total_votes = 0   # retrieve the total number of votes
+list_of_candidates = []     # retrieve the list of unique candidates
 
 with open(file_to_load, 'r') as election_data:
 
@@ -29,12 +30,26 @@ with open(file_to_load, 'r') as election_data:
         Open the data file.
         If applicable, read and ignore headers.
         Create a loop to increment the total number of votes.
+
+    Pseudocode for a complete list of candidates who received votes:
+        Create a variable to store the list of unique candidates.
+        Open the data file.
+        If applicable, read and ignore headers.
+        Create a loop to read each row, and only care about the 3rd column.
     """
 
     # Read each row from the file reader 1 at a time
     for row in file_reader:
         total_votes += 1
+        # print(row)
 
-print(f'The total number of voites cast is {total_votes}')
+        # Each row is a list, and we want the candidate, which is on index 2
+        if row[2] not in list_of_candidates:
+            list_of_candidates.append(row[2])
+
+
+print(f'The total number of votes cast is {total_votes}')
+print(f'The list of candidates: {list_of_candidates}')
+
 
     
